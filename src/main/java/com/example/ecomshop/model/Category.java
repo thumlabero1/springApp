49966b2ea8138e5713911b4862d9d7003b5de758
Phan_Linh <nhomtoanhsg20172018@gmail.com>
2022-12-09 13:17:58 +0700

    add getter and setter

--- src/main/java/com/example/ecomshop/model/Category.java
+++ src/main/java/com/example/ecomshop/model/Category.java
@@ -11,5 +11,17 @@ public class Category {
     @GeneratedValue(strategy = GenerationType.AUTO)
     @Column(name = "category_id")
     private int id;
-    private String name;
+    public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	private String name;
 }
--- src/main/java/com/example/ecomshop/model/User.java
+++ src/main/java/com/example/ecomshop/model/User.java
@@ -15,7 +15,43 @@ public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Integer id;
-    @NotEmpty
+    public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public List<Role> getRoles() {
+		return roles;
+	}
+	public void setRoles(List<Role> roles) {
+		this.roles = roles;
+	}
+	@NotEmpty
     @Column(nullable = false)
     private String firstName;
     private String lastName;
